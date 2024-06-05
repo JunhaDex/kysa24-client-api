3166270cd9238356f8bccacb1749d0337eabffda
JunhaDex <dk.junha.dev@gmail.com>
2024-06-05 10:42:07 +0900

    add common resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './resources/user/user.module';
+import { PostModule } from './resources/post/post.module';
+import { GroupModule } from './resources/group/group.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PostModule, GroupModule],
   controllers: [AppController],
   providers: [AppService],
 })
